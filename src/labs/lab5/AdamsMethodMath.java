b3e6ed1ea0a92f5d93e9440cc050f312fd6a95d1
Kirill <kirill.shakhov14@gmail.com>
2021-05-18 11:31:02 +0300

    add AdamsMethodMath(dont work)(lab5)

--- src/labs/lab5/AdamsMethodMath.java
+++ src/labs/lab5/AdamsMethodMath.java
@@ -3,45 +3,48 @@ import labs.models.Point;
 import java.util.ArrayList;
 import java.util.Scanner;
 
+import static labs.lab5.RungeKuttaMethodMath.rungeKutta;
+
 
 public class AdamsMethodMath {
 
-    double F(double x, double y) // y'
+    public static double F(double x, double y) // y'
     {
         return 4 * Math.cos(x) + Math.sin(2 * x);
     }
 
     //Метод Рунге - Кутты 4 порядка
-    public ArrayList<Point> RungeKuttaMethod(double a1, double a2, double b, int h, int n)
+    public static ArrayList<Point> RungeKuttaMethod(double a1, double a2, double b, double h, int n)
     {
-        double[] Y1 = new double[n];
-        double[] Y2 = new double[n];
-        double[] Y3 = new double[n];
-        double[] Y4 = new double[n];
-        double[] X = new double[n], Y = new double[n];
-        X[0] = a1; Y[0] = a2;
+        double[] k1 = new double[n+1];
+        double[] k2 = new double[n+1];
+        double[] k3 = new double[n+1];
+        double[] k4 = new double[n+1];
+        double[] x = new double[n+1];
+        double[] y = new double[n+1];
+        x[0] = a1; y[0] = a2;
         for (int i = 1; i <= n; i++)
         {
-            X[i] = a1 + i * h;
-            Y1[i] = h * F(X[i - 1], Y[i - 1]);
-            Y2[i] = h * F(X[i - 1] + h / 2.0, Y[i - 1] + Y1[i] / 2.0);
-            Y3[i] = h * F(X[i - 1] + h / 2, Y[i - 1] + Y2[i] / 2);
-            Y4[i] = h * F(X[i - 1] + h, Y[i - 1] + Y3[i]);
-            Y[i] = Y[i - 1] + (Y1[i] + 2 * Y2[i] + 2 * Y3[i] + Y4[i]) / 6;
+            x[i] = a1 + i * h;
+            k1[i] = h * F(x[i - 1], y[i - 1]);
+            k2[i] = h * F(x[i-1] + 0.5 * h, y[i-1] + 0.5 * k1[i]);
+            k3[i] = h * F(x[i-1] + 0.5 * h, y[i-1] + 0.5 * k2[i]);
+            k4[i] = h * F(x[i-1] + h, y[i-1] + k3[i]);
+            y[i] = y[i-1] + (k1[i]+2 * k2[i]+2 * k3[i]+k4[i])/6;
         }
         ArrayList<Point> points = new ArrayList<>();
         for(int i = 0; i < n; i++){
-            points.add(new Point(X[i], Y[i]));
+            points.add(new Point(x[i], y[i]));
         }
         return points;
     }
 
     //Метод Адамса
-    ArrayList<Point> AdamsMethod(double a1, double a2, double b, double h, int n)
+    public static ArrayList<Point> AdamsMethod(double a1, double a2, double b, double h, int n)
     {
         double k1, k2, k3, k4;
-        double[] f = new double[n];
-        double[] X = new double[n], Y = new double[n];
+        double[] f = new double[n+1];
+        double[] X = new double[n+1], Y = new double[n+1];
         X[0] = a1; Y[0] = a2;
         f[0] = F(X[0], Y[0]);
         for (int i = 0; i <= n; i++)
@@ -68,12 +71,12 @@ public class AdamsMethodMath {
         return points;
     }
 
-    public void solve() {
+    public static void solve() {
         Scanner scanner = new Scanner(System.in);
         System.out.println("Введите начальное условие задачи Коши (y(0)):");
         double y0 = Double.parseDouble(scanner.nextLine());
         System.out.println("Введите шаг:");
-        int h = Integer.parseInt(scanner.nextLine());
+        double h = Double.parseDouble(scanner.nextLine());
         System.out.println("Введите начало отрезка, на котором надо решить ДУ:");
         double x0 = Double.parseDouble(scanner.nextLine());
         System.out.println("Введите конец отрезка:");
@@ -84,6 +87,9 @@ public class AdamsMethodMath {
         for(Point p : points1){
             System.out.println("x: ["+p.getX()+"] y: ["+p.getY()+"]");
         }
+        System.out.println("Метод Рунге-Кутты 4 порядка(2 вариант):");
+        double ppp = rungeKutta((x, y) -> 4 * Math.cos(x) + Math.sin(2 * x), x0, y0, end, h);
+        System.out.println(ppp);
         System.out.println("Метод Адамса:");
         ArrayList<Point> points2 = AdamsMethod(x0, y0, end, h, n);
         for(Point p : points2){
